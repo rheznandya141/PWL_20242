63649f9befa85f71c912e08743edd0b6a8d9b7ac
rheznandya <nandyanandya3@gmail.com>
2024-08-26 19:01:46 +0700

    modifikasi tampilan awal

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,13 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PWL 2024</title>
+    <title>PWL 20242</title>
 </head>
 <body>
-    <h2>Pemrograman Web Lanjut 2024</h2>
-    <h3>Semangat Belajar Laravel 10</h3>
+    <h2>Pemrograman Web Lanjut 20242</h2>
+    <h3>Semangat Belajar Laravel 11</h3>
 </body>
 </html>
+
+
+
